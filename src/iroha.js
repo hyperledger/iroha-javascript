15e757dcf4fff03b495b3cd1f57c652f0d140b85
GUCCI-swallow <swallow965dev@gmail.com>
2016-09-23 13:19:27 +0900

    add register

--- src/iroha.js
+++ src/iroha.js
@@ -1,9 +1,11 @@
-"use strict"
+"use strict";
 var supercop = require('supercop.js');
+var superagent = require('superagent');
+var sha3 = require('');
 
 var iroha = {};
 
-iroha.createKeyPair = function(){
+var createKeyPair = function(){
     var seed = supercop.createSeed();
     var keys = supercop.createKeyPair(seed);
 
@@ -13,15 +15,51 @@ iroha.createKeyPair = function(){
     }
 }
 
-iroha.register = function(opt){
-    if(opt.accessPoint && opt.name)return false;
+var saveKeyPair = function(keyPair){
+    if(!keyPair)return false;
+    setLocalStorage({key: "keys",value: JSON.stringify(keyPair)});
+}
 
+var getTimeStanp = function(){
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = ("0" + (date.getMonth() + 1)).slice(-2);
+    var day = ("0" + date.getDate()).slice(-2);
+    var hour = ("0" + date.getHours()).slice(-2);
+    var min = ("0" + date.getMinutes()).slice(-2);
+    var sec = ("0" + date.getSeconds()).slice(-2);
+
+    var dateFormat = year + "-" + month + "-" + day + "T" + hour + ":" + min + ":" + sec; 
+
+    return dateFormat;
+}
+
+
+iroha.register = function(opt, callback){
+    if(opt.accessPoint && opt.name)return false;
     var accessPoint = opt.accessPoint;
     var name = opt.name;
 
+    setLocalStorage({key: "accessPoint",value: opt.accessPoint});
+
+    var keyPair = createKeyPair();
+    saveKeyPair(keyPair);
+
+    var param = {
+        "publicKey": keyPair.publicKey,
+        "screen_name": name,
+        "timestanp": getTimeStanp()
+    }
+
+    postRequest(accessPoint + "/account/register", param).then(function(res){
+        return res;
+    }).catch(function(err){
+        console.log(err);
+    });
+
 }
 
-iroha.createSignature = function(){
+var createSignature = function(){
 
 }
 
@@ -38,19 +76,53 @@ iroha.getTransaction = function(uuid){
 }
 
 
-
 var getLocalStorage = function(data){
-    if(data.key && data.value)return false;
+    if(!data.key && !data.value)return false;
     var storage = localStorage;
     storage.setItem(data.key, data.value);
 }
 
-var setLocalStorage = function(data){
-    if(data.key)return false;
+var setLocalStorage = function(key){
+    if(!key)return false;
     var storage = localStorage;
     return storage.getItem(data.key);
 }
 
+
+var getRequest = function(opt){
+    return new Promise(function(resolve, reject){
+        if(!opt.url && !callback)return false;
+
+        superagent
+            .get(opt.url)
+            .end(function(err, res){
+                if(!err){
+                    resolve(res);
+                    return;
+                }
+
+                reject(err);
+            });
+    });
+}
+
+var postRequest = function(opt){
+    return new Promise(function(resolve, reject){
+        if(!opt.url && !opt.param)return false;
+
+        superagent
+            .post(opt.url)
+            .send(opt.param)
+            .end(function(err, res){
+                if(!err){
+                    resolve(res);
+                    return;
+                }
+
+                reject(err);
+            });
+    });
+}
 
 
 module.export = iroha;
