fc8f43122f8900f2f3fa4e19031d51f7121c934e
George Theofilis <theofilis.g@gmail.com>
2017-03-18 19:23:43 +0200

    docs($compile): Update README.md

--- src/irohajs.ts
+++ src/irohajs.ts
@@ -1,35 +1,35 @@
-const sha3_256 = require('js-sha3').sha3_256
-const supercop = require('supercop.js')
+const sha3_256 = require("js-sha3").sha3_256
+const supercop = require("supercop.js")
 
 export function createKeyPair (): IKeyPair {
   const seed = supercop.createSeed()
   const keys = supercop.createKeyPair(seed)
 
   return {
-    publicKey: keys.publicKey.toString('base64'),
-    privateKey: keys.secretKey.toString('base64')
+    publicKey: keys.publicKey.toString("base64"),
+    privateKey: keys.secretKey.toString("base64")
   }
 }
 
 export function sign (opt: { publicKey: string, privateKey: string, message: string }): string {
-  const publicKey = new Buffer(opt.publicKey, 'base64')
-  const privateKey = new Buffer(opt.privateKey, 'base64')
+  const publicKey = new Buffer(opt.publicKey, "base64")
+  const privateKey = new Buffer(opt.privateKey, "base64")
   const sha3Message = new Buffer(sha3_256(opt.message))
 
   const sig = supercop.sign(
     sha3Message,
     publicKey,
     privateKey
-  ).toString('base64')
+  ).toString("base64")
 
   return sig
 }
 
 export function verify (opt: { publicKey: string, message: string, signature: string }) {
   const valid = supercop.verify(
-    new Buffer(opt.signature, 'base64'),
+    new Buffer(opt.signature, "base64"),
     new Buffer(opt.message),
-    new Buffer(opt.publicKey, 'base64')
+    new Buffer(opt.publicKey, "base64")
   )
   return valid
 }
@@ -58,13 +58,13 @@ export class Wallet implements IWallet {
 
   toJSON (): IKeyPair {
     return {
-      publicKey: this.publicKey.toString('base64'),
-      privateKey: this.privateKey.toString('base64')
+      publicKey: this.publicKey.toString("base64"),
+      privateKey: this.privateKey.toString("base64")
     }
   }
 
   sign (msg: string): string {
     const message = new Buffer(sha3_256(msg))
-    return supercop.sign(message, this.publicKey, this.privateKey).toString('base64')
+    return supercop.sign(message, this.publicKey, this.privateKey).toString("base64")
   }
 }
--- test/irohajs.test.ts
+++ test/irohajs.test.ts
@@ -1,66 +1,66 @@
-import * as iroha from '../src/irohajs'
-const sha3_256 = require('js-sha3').sha3_256
-const supercop = require('supercop.js')
+import * as iroha from "../src/irohajs"
+const sha3_256 = require("js-sha3").sha3_256
+const supercop = require("supercop.js")
 
-describe('TEST Iroha javascript', () => {
-  const publicKey = 'qzesd0bqIFfx1RQDS2HgcG5DQq/7DYF8u1YfMHAeJUs='
-  const privateKey = 'MEjhWDSSeQobXA55uYR+EFxEQOoQ68db59Gi1ocQ7ljWZNNQM35MJQpst83FcCKI1hra/53IxeZijEUIiM/Reg=='
-  const signature = 'wg8rlcWHe+PjkmJDllfMIF4PYUEbvNJ/yaoTmWzQUFe3mjnvuXnG3UWwNJfn47ms670sMvELXcliugBBhgeoCA=='
+describe("TEST Iroha javascript", () => {
+  const publicKey = "qzesd0bqIFfx1RQDS2HgcG5DQq/7DYF8u1YfMHAeJUs="
+  const privateKey = "MEjhWDSSeQobXA55uYR+EFxEQOoQ68db59Gi1ocQ7ljWZNNQM35MJQpst83FcCKI1hra/53IxeZijEUIiM/Reg=="
+  const signature = "wg8rlcWHe+PjkmJDllfMIF4PYUEbvNJ/yaoTmWzQUFe3mjnvuXnG3UWwNJfn47ms670sMvELXcliugBBhgeoCA=="
 
   beforeEach((done) => {
     done()
   })
 
-  describe('Iroha Create new KeyPair', () => {
-    it('Create new KeyPair!', () => {
+  describe("Iroha Create new KeyPair", () => {
+    it("Create new KeyPair!", () => {
       const keyPair = iroha.createKeyPair()
       expect(keyPair).toBeDefined()
     })
   })
 
-  describe('Iroha Create new Wallet', () => {
-    it('Create new Wallet!', () => {
+  describe("Iroha Create new Wallet", () => {
+    it("Create new Wallet!", () => {
       const wallet = new iroha.Wallet()
       expect(wallet.toJSON()).toBeDefined()
     })
 
-    it('Create new Wallet!', () => {
+    it("Create new Wallet!", () => {
       const wallet = new iroha.Wallet({
-        privateKey: new Buffer(privateKey, 'base64'),
-        publicKey: new Buffer(publicKey, 'base64')
+        privateKey: new Buffer(privateKey, "base64"),
+        publicKey: new Buffer(publicKey, "base64")
       })
-      expect(wallet.privateKey.toString('base64')).toBe(privateKey)
-      expect(wallet.publicKey.toString('base64')).toBe(publicKey)
+      expect(wallet.privateKey.toString("base64")).toBe(privateKey)
+      expect(wallet.publicKey.toString("base64")).toBe(publicKey)
     })
 
-    it('Check signature', () => {
+    it("Check signature", () => {
       const wallet = new iroha.Wallet({
-        privateKey: new Buffer(privateKey, 'base64'),
-        publicKey: new Buffer(publicKey, 'base64')
+        privateKey: new Buffer(privateKey, "base64"),
+        publicKey: new Buffer(publicKey, "base64")
       })
-      const sig = wallet.sign('test')
+      const sig = wallet.sign("test")
       expect(sig).toBe(signature)
     })
   })
 
-  describe('Iroha CreateSignature', () => {
-    it('Signature succeeded!', () => {
-      const msg = 'test'
+  describe("Iroha CreateSignature", () => {
+    it("Signature succeeded!", () => {
+      const msg = "test"
       const sig = iroha.sign({
-        'publicKey': publicKey,
-        'privateKey': privateKey,
-        'message': msg
+        "publicKey": publicKey,
+        "privateKey": privateKey,
+        "message": msg
       })
 
       expect(sig).toBe(signature)
     })
 
-    it('Signature not succeeded!', () => {
-      const msg = 'abcd'
+    it("Signature not succeeded!", () => {
+      const msg = "abcd"
       const sig = iroha.sign({
-        'publicKey': publicKey,
-        'privateKey': privateKey,
-        'message': msg
+        "publicKey": publicKey,
+        "privateKey": privateKey,
+        "message": msg
       })
 
       expect(sig).not.toBe(signature)
@@ -68,24 +68,24 @@ describe('TEST Iroha javascript', () => {
 
   })
 
-  describe('Iroha Verify', () => {
-    it('Verify succeeded!', () => {
-      const msg = sha3_256('test')
+  describe("Iroha Verify", () => {
+    it("Verify succeeded!", () => {
+      const msg = sha3_256("test")
       const res = iroha.verify({
-        'publicKey': publicKey,
-        'message': msg,
-        'signature': signature
+        "publicKey": publicKey,
+        "message": msg,
+        "signature": signature
       })
 
       expect(res).toBeTruthy()
     })
 
-    it('Verify not succeeded!', () => {
-      const msg = sha3_256('abcd')
+    it("Verify not succeeded!", () => {
+      const msg = sha3_256("abcd")
       const res = iroha.verify({
-        'publicKey': publicKey,
-        'message': msg,
-        'signature': signature
+        "publicKey": publicKey,
+        "message": msg,
+        "signature": signature
       })
 
       expect(res).not.toBeTruthy()
