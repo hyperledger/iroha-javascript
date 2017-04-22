d2d48137c75bc6006fbc4bf780ec7cc28f84c6e4
George Theofilis <theofilis.g@gmail.com>
2017-04-22 19:28:08 +0300

    test: Refactor test in order to use new Iroha service facade

--- test/asset.test.ts
+++ test/asset.test.ts
@@ -1,6 +1,5 @@
 import * as iroha from "../src/irohajs";
-import { AssetResponse } from "../src/api";
-import { IROHA_HOST } from "./config";
+import {request, assertHelperData} from "./__mocks__/request";
 
 describe("TEST Iroha Asset Repository", () => {
   beforeEach((done) => {
@@ -8,36 +7,25 @@ describe("TEST Iroha Asset Repository", () => {
   });
 
   describe("AssetRepository", () => {
-    let proto: iroha.IApi;
     let client: iroha.IAssetRepositoryService;
 
     beforeAll(() => {
-      proto = iroha.grpc.load("src/protos/api.proto");
-      client = new proto.Api.AssetRepository(IROHA_HOST, iroha.grpc.credentials.createInsecure());
+      const service = new iroha.IrohaService();
+      client = service.AssetRepository;
     });
 
     it("find!", () => {
       const query = new iroha.Query();
-      return new Promise((resolve, reject) => {
-        client.find(query, (err, response) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(response);
-          }
-        });
-      }).then(data => {
-        expect(data).toEqual({
-          "account": null,
-          "asset": null,
-          "code": "0",
-          "domain": null,
-          "message": "OK",
-          "peer": null,
-          "simpleAsset": null,
-          "timestamp": "0"
-        });
-      });
+      return request(client, "find", [query]).then(assertHelperData({
+        "account": null,
+        "asset": null,
+        "code": "0",
+        "domain": null,
+        "message": "OK",
+        "peer": null,
+        "simpleAsset": null,
+        "timestamp": "0"
+      }));
     });
   });
 });
