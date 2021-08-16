0ef54e6fb09ffc2b622c85d021d16cd4e0498437
0x009922 <a.marcius26@gmail.com>
2021-08-16 10:34:41 +0300

    debug: add debug logging
    Signed-off-by: 0x009922 <a.marcius26@gmail.com>

--- packages/iroha-test-peer/src/util.ts
+++ packages/iroha-test-peer/src/util.ts
@@ -1,9 +1,13 @@
 import execa from 'execa';
 import fs from 'fs/promises';
+import path from 'path';
+import { inspect } from 'util';
 
 export async function saveDataAsJSON(data: unknown, destination: string): Promise<void> {
-    console.log('saving json', data, destination);
     await fs.writeFile(destination, JSON.stringify(data), { encoding: 'utf-8' });
+
+    const dir = await fs.readdir(path.dirname(destination));
+    process.stdout.write(`JSON saved ${inspect({ dirAfter: dir, destination })}\n`);
 }
 
 export async function rmWithParams(
