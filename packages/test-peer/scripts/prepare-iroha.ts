8fd2ed43a56ea46dd462baaf00fdc23341dfa433
0x009922 <a.marcius26@gmail.com>
2021-10-10 13:28:43 +0300

    misc(test-peer): use gulp for workflows
    Signed-off-by: 0x009922 <a.marcius26@gmail.com>

--- packages/test-peer/scripts/prepare-iroha.ts
+++ packages/test-peer/scripts/prepare-iroha.ts
@@ -1,52 +1,46 @@
-import consola from 'consola';
 import execa from 'execa';
+import { series } from 'gulp';
 import path from 'path';
+import del from 'del';
+import makeDir from 'make-dir';
 import { TMP_IROHA_DEPLOY_DIR, TMP_IROHA_INSTALL_DIR } from '../const';
 
-function runAsyncMain(mainFn: () => Promise<void>): void {
-    mainFn().catch((err) => {
-        consola.fatal(err);
-        process.exit(1);
-    });
+const GIT_IROHA_REPO = 'https://github.com/hyperledger/iroha.git';
+const GIT_BRANCH = 'iroha2-dev';
+
+const ROOT_DIR = path.resolve(__dirname, '../');
+const IROHA_INSTALL_ROOT_DIR = path.resolve(ROOT_DIR, TMP_IROHA_INSTALL_DIR);
+
+const IROHA_DEPLOY_DIR = path.resolve(ROOT_DIR, TMP_IROHA_DEPLOY_DIR);
+
+async function install_iroha_binary() {
+    await execa(
+        'cargo',
+        [
+            'install',
+            '--root',
+            IROHA_INSTALL_ROOT_DIR,
+            '--git',
+            GIT_IROHA_REPO,
+            '--branch',
+            GIT_BRANCH,
+            // and finally - which crate from repo to install?
+            'iroha_cli',
+        ],
+        { stdio: 'inherit' },
+    );
 }
 
-async function $(file: string, args?: string[]): Promise<void> {
-    await execa(file, args, {
-        stdio: 'inherit',
-    });
+async function clean_deploy_dir() {
+    await del(path.join(IROHA_DEPLOY_DIR, '*'));
 }
 
-runAsyncMain(async () => {
-    const GIT_IROHA_REPO = 'https://github.com/hyperledger/iroha.git';
-    const GIT_BRANCH = 'iroha2-dev';
+async function make_deploy_dir() {
+    await makeDir(IROHA_DEPLOY_DIR);
+}
 
-    const workdir = path.resolve(__dirname, '../');
+async function copy_binary_into_deploy_dir() {
+    await execa('cp', [path.resolve(IROHA_INSTALL_ROOT_DIR, 'bin/iroha_cli'), IROHA_DEPLOY_DIR]);
+}
 
-    consola.info('Installing Iroha with Cargo from git repo');
-
-    const irohaInstallRootDir = path.resolve(workdir, TMP_IROHA_INSTALL_DIR);
-    await $('cargo', [
-        'install',
-        '--root',
-        irohaInstallRootDir,
-        '--git',
-        GIT_IROHA_REPO,
-        '--branch',
-        GIT_BRANCH,
-        // and finally - which crate from repo to install?
-        'iroha_cli',
-    ]);
-
-    consola.info('Extracting Iroha binary into deploy dir');
-
-    const deployDir = path.resolve(workdir, TMP_IROHA_DEPLOY_DIR);
-    // clean last deploy dir
-    await $('rm', ['-rf', deployDir]);
-    await $('mkdir', [deployDir]);
-    // copy
-    await $('cp', [path.resolve(irohaInstallRootDir, 'bin/iroha_cli'), deployDir]);
-
-    // ?????
-
-    consola.success('Done');
-});
+export default series(install_iroha_binary, clean_deploy_dir, make_deploy_dir, copy_binary_into_deploy_dir);
