ee72e673ea12b20e0c62317c1574da161e5f78f6
Dmitry Balashov <a.marcius26@gmail.com>
2023-01-24 09:56:00 +0700

    [refactor]: change `target-*` exports, add `wasmPkg`
    Signed-off-by: Dmitry Balashov <a.marcius26@gmail.com>

--- packages/crypto/packages/target-node/src/lib.ts
+++ packages/crypto/packages/target-node/src/lib.ts
@@ -1,3 +1,2 @@
-import * as crypto from '@iroha2/crypto-interface-wrap'
-
-export { crypto }
+export * as crypto from '@iroha2/crypto-interface-wrap'
+export * as wasmPkg from '~wasm-pkg/nodejs'
--- packages/crypto/packages/target-web/src/lib.ts
+++ packages/crypto/packages/target-web/src/lib.ts
@@ -1,3 +1,3 @@
-import * as crypto from '@iroha2/crypto-interface-wrap'
+export * as crypto from '@iroha2/crypto-interface-wrap'
+export * as wasmPkg from '~wasm-pkg/web'
 export { default as init } from '~wasm-pkg/web'
-export { crypto }
