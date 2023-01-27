dde80189bb1c42523eeddc60c0bd752b0563fbab
Dmitry Balashov <a.marcius26@gmail.com>
2023-01-27 08:13:51 +0700

    [test]: make crypto integration tests pass
    Signed-off-by: Dmitry Balashov <a.marcius26@gmail.com>

--- packages/crypto/test/integration/node/test/run.cjs
+++ packages/crypto/test/integration/node/test/run.cjs
@@ -1,4 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const { crypto } = require('@iroha2/crypto-target-node')
 
-const hash = crypto.createHash(Uint8Array.from([1, 2, 3, 4]))
+const hash = crypto.Hash.hash('hex', '0011224433').bytes('hex')
+console.log('Sample hash:', hash)
--- packages/crypto/test/integration/test-node/run.mjs
+++
@@ -1,3 +0,0 @@
-import { crypto } from '@iroha2/crypto-target-node'
-
-const hash = crypto.createHash(Uint8Array.from([1, 2, 3, 4]))
