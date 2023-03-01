148667dadd4681d39a901aea41736850106f6a43
Dmitry Balashov <a.marcius26@gmail.com>
2023-03-01 08:57:32 +0700

    [chore]: update reference to `iroha2-stable`
    Signed-off-by: Dmitry Balashov <a.marcius26@gmail.com>

--- packages/iroha-source/config.js
+++ packages/iroha-source/config.js
@@ -3,5 +3,6 @@
  */
 export default {
   origin: 'https://github.com/hyperledger/iroha.git',
-  rev: '753377ed0f02bf6a0430c280d82a24bb0602e121',
+  // `iroha2-stable` as of Feb 27 2023
+  rev: 'c4af68c4f7959b154eb5380aa93c894e2e63fe4e',
 }
