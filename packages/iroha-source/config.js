11d3bf9f2b1273dacbb632b8d9ee756b98a2bcc4
Dmitry Balashov <a.marcius26@gmail.com>
2023-10-25 14:44:41 +0700

    [fix]: update reference version
    Signed-off-by: Dmitry Balashov <a.marcius26@gmail.com>

--- packages/iroha-source/config.js
+++ packages/iroha-source/config.js
@@ -1,8 +1,9 @@
 /**
- * @type {import('./src/types').Configuration}
+ * @type {import('./src/types').RawConfig}
  */
 export default {
+  release: true,
   origin: 'https://github.com/hyperledger/iroha.git',
-  // `iroha2-dev` as of 17th July 2023 (pre rc 16-17)
-  rev: 'f958afef8c9561af61cc30aab9f8b3eb0c497fa4',
+  // `iroha2-stable` as of 17 Oct 2023 (RC20)
+  rev: '51d687607fad067fc855e266edc684d4fb33e7de',
 }
